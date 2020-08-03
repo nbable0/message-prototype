34c0ab1c5ecb710912637a490468c840c5a9b5ac
nickb <nickb@twintels-MBP.lan>
2020-08-03 08:15:58 -0700

    updating

---
+++ message-select.js
@@ -0,0 +1,23 @@
+//click a contact slat 
+const contactSlat = document.querySelectorAll('.contact-slat');
+const mainAvatar = document.querySelector('.header-av');
+const contactName = document.querySelectorAll('.name-clicked')
+const mainName = document.querySelector('.center-name')
+
+
+
+// contactSlat.forEach(item => {
+//     item.addEventListener('click', () => { 
+
+//         contactName.forEach(function () {
+//             mainName.innerText = contactName.innerText;
+//             console.log("hi")
+//         })
+        
+//   })
+// })
+
+//message container changes avatar and name
+
+
+//notes container changes avatar and name
